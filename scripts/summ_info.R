123ab1b221e1e208d47dfa90b1e9e95f8f241835
andreaargueta <rosal18@uw.edu>
2020-05-26 21:42:12 -0700

    andrea tries once again

--- scripts/summ_info.R
+++ scripts/summ_info.R
@@ -1,13 +1,13 @@
-## Summary Information on Global Coffee Insights
-## total number of countries that produced coffee
-country_prod <- (coffee_df) %>%
-  filter(Market_Year == 2019) %>%
-  nrow()
-##total number of Arabica coffee produced in 2019
-bean_imports <- coffee_df %>%
-  (coffee_df) %>%
-  filter(Market_Year == 2019) %>%
-  sum("Bean Imports")
-
-
-    
+library(dplyr)
+  # Data frame
+  df <- read.csv("../data/coffee_df.csv", stringsAsFactors = FALSE)
+  ## Summary Information on Global Coffee Insights
+  ## total number of countries that produced coffee
+  country_prod <- df %>%
+    filter(Market_Year == 2019) %>%
+    filter(Production != 0) %>%
+    nrow()
+  ##total number of Arabica coffee produced in 2019
+  new_df <- df %>%
+    filter(Market_Year == 2019)
+  bean_import <- sum(new_df$Bean.Imports, na.rm=TRUE)
