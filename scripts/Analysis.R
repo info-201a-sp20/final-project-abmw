17f3284681298fdbaeeaf7ecc9ee4cb3cc3c4d0e
Brynna Kilcline <bnk7@uw.edu>
2020-06-01 19:35:07 -0700

    revise consumption df again

--- scripts/Analysis.R
+++ scripts/Analysis.R
@@ -20,29 +20,20 @@ coffee_df <- raw_df %>%
 write.csv(coffee_df, "..\\data\\coffee_df.csv", row.names = FALSE)
 
 # CONSUMPTION DATAFRAME
-# make country names match world map data frame
+# make country names match
 fix_names <- function(col) {
   col <- col %>%
     str_replace("Congo (Brazzaville)", "Congo") %>%
     str_replace("Congo (Kinshasa)", "Congo, Democratic Republic of the") %>%
-    str_replace("United States", "United States of America") %>%
     str_replace("Congo, Rep.", "Congo") %>%
     str_replace("Congo, Dem. Rep.", "Congo, Democratic Republic of the") %>%
     str_replace("Egypt, Arab Rep.", "Egypt") %>%
-    str_replace("Iran, Islamic Rep.", "Iran (Islamic Republic of)") %>%
-    str_replace("Iran", "Iran (Islamic Republic of)") %>%
-    str_replace("Korea, Rep.", "Korea, Republic of") %>%
-    str_replace("South Korea", "Korea, Republic of") %>%
-    str_replace("Lao PDR", "Lao People's Democratic Republic") %>%
-    str_replace("Laos", "Lao People's Democratic Republic") %>%
+    str_replace("Iran, Islamic Rep.", "Iran") %>%
+    str_replace("Korea, Rep.", "South Korea") %>%
+    str_replace("Lao PDR", "Laos") %>%
     str_replace("Russia", "Russian Federation") %>%
     str_replace("Venezuela, RB", "Venezuela") %>%
-    str_replace("Yemen, Rep.", "Yemen") %>%
-    str_replace("CÃ´te d'Ivoire", "Cote d'Ivoire") %>%
-    str_replace("Taiwan", "Taiwan, Province of China") %>%
-    str_replace("Tanzania", "Tanzania, United Republic of") %>%
-    str_replace("Venezuela", "Venezuela (Bolivarian Republic of)") %>%
-    str_replace("Vietnam", "Viet Nam")
+    str_replace("Yemen, Rep.", "Yemen")
   return(col)
 }
 
@@ -51,8 +42,8 @@ pop_df <- read.csv("../data/populationdataworldbank.csv",
                    stringsAsFactors = F)
 
 pop_df <- pop_df %>%
-  select(-Country.Code, -Indicator.Code, -Indicator.Name, -X2019) %>%
-  gather(key = "Year", value = "Population", -ï..Country.Name)
+  select(-Indicator.Code, -Indicator.Name, -X2019) %>%
+  gather(key = "Year", value = "Population", -ï..Country.Name, -Country.Code)
 
 pop_df$Year <- pop_df %>%
   pull("Year") %>%
@@ -74,14 +65,6 @@ joined <- left_join(consumption_df, pop_df,
                            "Market_Year" = "Year")) %>%
   filter(Market_Year < 2019) %>%
   mutate(Consumption.per.Capita = (Domestic.Consumption / Population) * 1000)
-
-# add country codes
-codes <- read.csv("../data/country_codes.csv", stringsAsFactors = F) %>%
-  select(name, alpha.3)
-
-codes$name <- sapply(codes$name, fix_names)
-
-joined <- left_join(joined, codes, by = c("Country_Name" = "name"))
 
 # write
 write.csv(joined, file = "../data/dom_consumption_df.csv", row.names = F)
