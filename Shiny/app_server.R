f8788b619dcdf74552e5e9852aebe4bea2863fb1
Brynna Kilcline <bnk7@uw.edu>
2020-06-01 19:59:28 -0700

    add map to server

--- Shiny/app_server.R
+++ Shiny/app_server.R
@@ -1,4 +1,23 @@
 # Final-project-abmw app server script
+library("dplyr")
+library("plotly")
+library("tidyr")
+library("stringr")
+
 server <- function(input, output) {
-  
+  dom_consumption_df <- read.csv(
+    "../data/dom_consumption_df.csv", stringsAsFactors = F)
+  output$map <- renderPlot({ #output$map to be created in ui
+    dom_consumption_df <- dom_consumption_df %>%
+      filter(Market_Year == input$year) #input$year to be created in ui
+    
+    map <- plot_ly(dom_consumption_df, type = "choropleth",
+                   locations = dom_consumption_df$Country.Code,
+                   z = dom_consumption_df$Consumption.per.Capita,
+                   text = paste(dom_consumption_df$Country_Name),
+                   colorscale = "Browns") %>%
+      layout(title = "Domestic Consumption per Capita") %>%
+      colorbar(title = "Average Consumption per Person \n(in 60kg bags)")
+    return(map)
+  })
 }
