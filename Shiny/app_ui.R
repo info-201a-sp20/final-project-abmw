94f6ebbe437d2a1a5bbbe60f2bd10faa02f238b4
Brynna Kilcline <bnk7@uw.edu>
2020-06-03 13:01:58 -0700

    add image

--- Shiny/app_ui.R
+++ Shiny/app_ui.R
@@ -136,7 +136,7 @@ intro_page <- tabPanel(
   title = "Introduction",
   p(strong("Authors: Andrea Argueta, Mariam Khan, Brynna Kilcline, and William Zhang")),
   p(strong("Informatics 201-Section BA")),
-  img("The Stages of Coffee", src = "https://unsplash.com/photos/Y3AqmbmtLQI"),
+  img("The Stages of Coffee", src = "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"),
   h2("Overview"),
   p("While the world seems to be in a standstill,
    we thought we would choose an aspect of many people's 
